25eb1d24710d47347a8cc54621cea85e15c83061
hxmn <ilya@ashikhm.in>
2024-10-01 14:34:36 +0200

    release wheels

--- ndev/commands/release.py
+++ ndev/commands/release.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Any
 
 from cleo.commands.command import Command
+from cleo.io.outputs.output import Verbosity
 
 from ndev.shutil_ext import copytree_from_zip
 
@@ -21,6 +22,7 @@ class ReleaseCommand(Command):
         super().__init__()
         self.ndev_config: dict[str, Any] = {}
         self.project_file = CURRENT_DIR / "pyproject.toml"
+        self.requirements_txt_list: list[str] = []
 
     def handle(self) -> int:
         self._validate_env()
@@ -38,6 +40,7 @@ class ReleaseCommand(Command):
         return _project_dict["tool"]["ndev"]
 
     def copy_root(self):
+        self.line("Copying root directory.", verbosity=Verbosity.NORMAL)
         root_dir = CURRENT_DIR / self.ndev_config["release_root"]
         if not root_dir.exists():
             self.line_error(f"Root directory {root_dir} does not exist.")
@@ -53,8 +56,12 @@ class ReleaseCommand(Command):
         )
 
     def copy_local_files(self):
-        # copy local sources
+        self.line("Copying local files.", verbosity=Verbosity.NORMAL)
         for copy_item in self.ndev_config["copy-local"]:
+            self.line(
+                f"Copying {copy_item['from']} to {copy_item['to']}.", verbosity=Verbosity.VERBOSE
+            )
+
             src_path = CURRENT_DIR / copy_item["from"]
             if not src_path.exists():
                 self.line_error(f"Local source {src_path} does not exist.")
@@ -70,12 +77,13 @@ class ReleaseCommand(Command):
             )
 
     def generate_requirements_txt(self):
-        # generate requirements.txt
+        self.line("Generating requirements.txt.", verbosity=Verbosity.NORMAL)
         requirements_path = CURRENT_DIR / "requirements.txt"
         if not requirements_path.exists():
             result = subprocess.run(
                 "poetry export "
                 "--without-hashes "
+                "--with dev "
                 "--format requirements.txt "
                 "--output requirements.txt",
                 shell=True,
@@ -88,9 +96,7 @@ class ReleaseCommand(Command):
                 self.line_error(result.stderr)
                 return os.EX_NOINPUT
             else:
-                requirement_lines = requirements_path.read_text(
-                    encoding="utf8"
-                ).splitlines()
+                requirement_lines = requirements_path.read_text(encoding="utf8").splitlines()
                 requirement_lines = [
                     line
                     for line in requirement_lines
@@ -100,43 +106,73 @@ class ReleaseCommand(Command):
         if self.ndev_config["copy_requirements"]:
             shutil.copy2(src=requirements_path, dst=SOURCES_DIR / "requirements.txt")
 
-    def download_wheels(self):
-        result = subprocess.run(
-            f"pip download "
-            "--requirement requirements.txt "
-            "--no-deps "
-            "--exists-action i "
-            "--platform manylinux_2_28_x86_64 "
-            f'--dest "{WHEELS_DIR}" '
-            "--quiet",
-            shell=True,
-            capture_output=True,
-            text=True,
-        )
-        if result.returncode != os.EX_OK:
-            self.line_error("Failed to download wheels.")
-            self.line_error(result.stdout)
-            self.line_error(result.stderr)
-            return os.EX_NOINPUT
+        self.requirements_txt_list = requirements_path.read_text(encoding="utf8").splitlines()
 
-    def copy_wheels_sources(self):
-        all_wheels_files = list(WHEELS_DIR.glob("*.whl")) + list(
-            WHEELS_DIR.glob("*.tar.gz")
-        )
+    def download_wheels(self):
+        self.line(text="Downloading wheels: ", verbosity=Verbosity.NORMAL)
 
         for copy_item in self.ndev_config["copy-wheel-src"]:
-            wheel_file = next(
-                (
-                    f
-                    for f in all_wheels_files
-                    if f.name.startswith(copy_item["from"] + "-")
-                ),
+            wheel_name = copy_item["from"]
+            wheel_name = wheel_name.replace("_", "-")
+            requirement_spec = next(
+                (line for line in self.requirements_txt_list if f"{wheel_name}==" in line),
                 None,
+            )
+            if ";" in requirement_spec:
+                requirement_spec = requirement_spec.split(";")[0]
+            self.line(
+                f"Downloading wheel: {wheel_name}, spec: {requirement_spec}",
+                verbosity=Verbosity.VERBOSE,
+            )
+            if requirement_spec is None:
+                self.line_error(f"Requirement {copy_item['from']} not found in requirements.txt.")
+                return os.EX_NOINPUT
+
+            result = subprocess.run(
+                f"pip download "
+                "--no-deps "
+                "--ignore-requires-python "
+                "--exists-action i "
+                "--platform manylinux_2_28_x86_64 "
+                f" {requirement_spec} "
+                f'--dest "{WHEELS_DIR}" ',
+                capture_output=True,
+                text=True,
+                shell=True,
+            )
+
+            if result.returncode != os.EX_OK:
+                self.line_error(f"Failed to download wheels. Status: {result.returncode}")
+                self.line_error(f"stdout: [{result.stdout}]")
+                self.line_error(f"stderr: [{result.stderr}]")
+                return result.returncode
+
+            if self.io.is_verbose():
+                self.line(result.stderr)
+                self.line(result.stdout)
+
+    def copy_wheels_sources(self):
+        self.line(text=f"Copying wheel sources to {SOURCES_DIR}.", verbosity=Verbosity.NORMAL)
+        all_wheels_files = list(WHEELS_DIR.glob("*.whl")) + list(WHEELS_DIR.glob("*.tar.gz"))
+
+        for copy_item in self.ndev_config["copy-wheel-src"]:
+            self.line(
+                f"Copying wheel {copy_item['from']} to {copy_item['to']}.",
+                verbosity=Verbosity.VERBOSE,
+            )
+            wheel_file = next(
+                (f for f in all_wheels_files if f.name.startswith(copy_item["from"] + "-")),
+                None,
+            )
+            self.line(
+                f"Copying {wheel_file} to {SOURCES_DIR / copy_item['to']}.",
+                verbosity=Verbosity.VERBOSE,
             )
             copytree_from_zip(
                 zip_path=wheel_file,
                 dst_dir=SOURCES_DIR / copy_item["to"],
                 path_in_zip=copy_item["from"],
+                ignore=shutil.ignore_patterns("*.so"),
             )
 
     def _validate_env(self):
--- ndev/shutil_ext.py
+++ ndev/shutil_ext.py
@@ -8,16 +8,23 @@ def copytree_from_zip(
     zip_path: Path,
     dst_dir: Path,
     path_in_zip: str = ".",
+    ignore=None,
 ) -> None:
     """Extracts a directory from a ZIP archive to the destination directory.
 
     Args:
-        zip_path: Path to the ZIP archive.
-        dst_dir: Destination directory.
-        path_in_zip: Path to the directory in the ZIP archive.
+        :param zip_path: Path to the ZIP archive.
+        :param dst_dir: Destination directory.
+        :param path_in_zip: Path to the directory in the ZIP archive.
+        :param ignore: Ignore path-callable
     """
     with zipfile.ZipFile(zip_path, "r") as zip_file:
         with tempfile.TemporaryDirectory() as tmp_dir:
             zip_file.extractall(tmp_dir)
             src_dir = Path(tmp_dir) / path_in_zip
-            shutil.copytree(src=src_dir, dst=dst_dir, dirs_exist_ok=True)
+            shutil.copytree(
+                src=src_dir,
+                dst=dst_dir,
+                dirs_exist_ok=True,
+                ignore=ignore,
+            )
